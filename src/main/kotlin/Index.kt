ecb7ae6dd67cccf3c2f77e0a0c92fc72401dda4d
Verachad Wongsawangtham <birth24987@gmail.com>
2017-10-11 21:36:47 +0700

    Add promise example

--- src/main/kotlin/Index.kt
+++ src/main/kotlin/Index.kt
@@ -1,3 +1,5 @@
+import kotlin.js.Promise
+
 external fun require(module: String): dynamic
 external val exports: dynamic
 
@@ -12,6 +14,22 @@ fun main(args: Array<String>) {
         val p = Package("package1", 20, listOf(Item("A", ItemType.CAR), Item("B", ItemType.FURNITURE)))
         res.status(200).send(p)
     }
+
+    exports.promise = functions.https.onRequest { req, res ->
+        Promise<Double>({ resolve, _ ->
+            val piValue = (0..req.query.number).sumByDouble {
+                val num = if(it % 2L == 0L) -1.0 else 1.0
+                num / (2 * it + 1)
+            }
+            resolve(piValue)
+        }).then({
+            console.log("value $it")
+            res.status(200).send(it.toString())
+        }).catch {
+            console.log("error: $it")
+        }
+    }
+
 }
 
 enum class ItemType {
